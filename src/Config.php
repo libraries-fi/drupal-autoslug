9f3a550043099d607395f2a7aebb8838699b54dd
Samu Juvonen <samu.juvonen@kirjastot.fi>
2017-02-15 14:26:50 +0200

    Implement generic support for entities

--- src/Config.php
+++ src/Config.php
@@ -4,18 +4,17 @@ namespace Drupal\autoslug;
 
 use Drupal\node\Entity\Node;
 
-class Config
-{
-    public function __construct($config)
-    {
-        $this->config = $config->get('autoslug.settings');
-    }
+class Config {
+  public function __construct($config) {
+    $this->config = $config->get('autoslug.settings');
+  }
 
-    public function configForEntity($entity)
-    {
-        if ($entity instanceof Node) {
-            $key = sprintf('node.%s', $entity->bundle());
-            return $this->config->get($key);
-        }
-    }
+  public function configForEntity($entity, $langcode) {
+    $type_id = $entity->getEntityTypeId();
+    $bundle_id = $entity->bundle();
+    $key = sprintf('%s.%s', $type_id, $bundle_id);
+    $key_lang = sprintf('%s.%s', $key, $langcode);
+
+    return $this->config->get($key_lang) ?: $this->config->get($key);
+  }
 }
