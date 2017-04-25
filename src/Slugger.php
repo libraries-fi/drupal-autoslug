335b25b595cb60e3b06544f84046673d6abb968e
Samu Juvonen <samu.juvonen@kirjastot.fi>
2017-04-25 14:04:46 +0300

    Parameter for defining maximum number of words in slugified string

--- src/Slugger.php
+++ src/Slugger.php
@@ -5,7 +5,7 @@ namespace Drupal\autoslug;
 class Slugger {
   protected $aliasStorage;
 
-  public static function slugify($string, $randomize = FALSE) {
+  public static function slugify($string, $randomize = FALSE, $max_words = 7) {
     $string = mb_strtolower(trim($string));
     $string = str_replace(['ä', 'ö', 'å'], ['a', 'o', 'a'], $string);
     $string = preg_replace('/[\s]+/', '-', $string);
@@ -14,6 +14,11 @@ class Slugger {
 
     if ($randomize) {
       $string .= '-' . substr(uniqid(true), -5);
+    }
+
+    if ($max_words > 0) {
+      $words = array_slice(explode('-', $string), 0, $max_words);
+      $string = implode('-', $words);      
     }
 
     return $string;
