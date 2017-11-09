7867aa3043698d9f1d01312fbbb720d9cde0fe4f
Samu Juvonen <samu.juvonen@kirjastot.fi>
2017-11-09 17:05:44 +0200

    Static caching for transliterators

--- src/Slugger.php
+++ src/Slugger.php
@@ -5,15 +5,30 @@ namespace Drupal\autoslug;
 use Transliterator;
 
 class Slugger {
-  public static function slugify($string, $randomize = FALSE, $max_words = 7) {
-    if (!stripos($string, 'a') && !stripos('e', $string) && !stripos('i', $string)) {
+  public static $converters = [];
+
+  public static function transliterate($string) {
+    if (empty(self::$converters)) {
       $scripts = ['Cyrillic', 'Katakana', 'Hiragana', 'Hangul', 'Thai', 'Arabic', 'Syriac', 'Armenian', 'Bengali'];
+
       foreach ($scripts as $script) {
         if ($tr = Transliterator::create(sprintf('%s-Latin', $script))) {
-          $string = $tr->transliterate($string);
-          $string = preg_replace('/(\W+)/', ' $1 ', $string);
+          self::$converters[] = $tr;
         }
       }
+    }
+
+    foreach (self::$converters as $tr) {
+      $string = $tr->transliterate($string);
+      $string = preg_replace('/(\W+)/', ' $1 ', $string);
+    }
+
+    return $string;
+  }
+
+  public static function slugify($string, $randomize = FALSE, $max_words = 7) {
+    if (!stripos($string, 'a') && !stripos('e', $string) && !stripos('i', $string)) {
+      $string = self::transliterate($string);
     }
 
     $string = mb_strtolower(trim($string));
